9f6778bdec5df134a00f9033feddaa1e9b4afb1e
Alexander Yakusik <alexander.yakusik@gmail.com>
2020-02-23 15:59:21 +0300

    Added notifications. Implemented creation of users for super user.

---
+++ EShop/Infrastructure/Extensions/TempDataDictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using EShop.Models.App;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+
+namespace EShop.Infrastructure.Extensions
+{
+    public static class TempDataDictionaryExtensions
+    {
+        private const string TEMP_DATA_NOTIFICATION_MESSAGE_KEY = "NOTIFICATION_MESSAGE";
+
+        public static void AddNotificationMessage(this ITempDataDictionary tempData, Notification notification)
+        {
+            string json = JsonConvert.SerializeObject(notification);
+            tempData[TEMP_DATA_NOTIFICATION_MESSAGE_KEY] = json;
+        }
+
+        public static string GetNotificationMessage(this ITempDataDictionary tempData)
+        {
+            return tempData[TEMP_DATA_NOTIFICATION_MESSAGE_KEY] as string;
+        }
+    }
+}
---
+++ EShop/wwwroot/js/notification.js
@@ -0,0 +1,36 @@
+﻿(() => {
+    document.addEventListener('DOMContentLoaded', e => {
+        const notification = getNotificationMessage();
+        notify(notification);
+    });
+
+    function getNotificationMessage() {
+        const script = document.getElementById('notification');
+        const data = script.dataset['message'];
+        if (data) {
+            return JSON.parse(data);
+        }
+
+        return null;
+    }
+
+    function notify(notification) {
+        if (!notification) {
+            return;
+        }
+
+        switch (notification.Type) {
+            case "Success":
+                toastr.success(notification.Message);
+                break;
+            case "Error":
+                toastr.error(notification.Message);
+                break;
+            case "Info":
+                toastr.info(notification.Message);
+                break;
+            default:
+                break;
+        }
+    }
+})();

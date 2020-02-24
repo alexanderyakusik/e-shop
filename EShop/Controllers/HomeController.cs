2def1e596ee0811026383978e556888fab988acc
Alexander Yakusik <alexander.yakusik@gmail.com>
2020-02-24 03:57:35 +0300

    Added cart items interactions

--- EShop/Controllers/HomeController.cs
+++ EShop/Controllers/HomeController.cs
@@ -1,22 +1,28 @@
-﻿using EShop.Models;
+﻿using EShop.Data;
+using EShop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace EShop.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Good.Where(good => good.InCatalog).ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ EShop/wwwroot/js/catalog.js
@@ -0,0 +1,82 @@
+﻿(() => {
+    document.addEventListener('DOMContentLoaded', e => {
+        const cart = window.cart;
+        if (!cart) {
+            return;
+        }
+
+        initializeCartAdd(cart, true);
+        initializeCartRemove(cart, true);
+        initializeClearCart(cart);
+    });
+
+    function initializeCartAdd(cart, withListeners) {
+        const links = document.querySelectorAll('button[data-cart="add"]');
+
+        for (let link of links) {
+            const id = parseInt(link.dataset['id']);
+            const max = parseInt(link.dataset['max']);
+
+            const items = cart.getItems();
+            if (id in items) {
+                link.innerText = `Add to cart (${items[id]})`;
+
+                if (items[id] === max) {
+                    link.disabled = true;
+                }
+            } else {
+                link.disabled = false;
+                link.innerText = 'Add to cart';
+            }
+
+            if (withListeners) {
+                link.addEventListener('click', _ => {
+                    cart.add({ id });
+
+                    const items = cart.getItems();
+
+                    link.innerText = `Add to cart (${items[id]})`;
+
+                    if (items[id] === max) {
+                        link.disabled = true;
+                    }
+
+                    initializeCartRemove(cart, false);
+                });
+            }
+        }
+    }
+
+    function initializeCartRemove(cart, withListeners) {
+        const links = document.querySelectorAll('button[data-cart="remove"]');
+
+        for (let link of links) {
+            const id = parseInt(link.dataset['id']);
+
+            const items = cart.getItems();
+            link.disabled = !(id in items);
+
+            if (withListeners) {
+                link.addEventListener('click', _ => {
+                    cart.remove({ id });
+                    link.disabled = true;
+
+                    initializeCartAdd(cart, false);
+                });
+            }
+        }
+    }
+
+    function initializeClearCart(cart) {
+        const clearCart = document.getElementById('clear-cart');
+
+        if (!clearCart) { return; }
+
+        clearCart.addEventListener('click', () => {
+            cart.clear();
+
+            initializeCartAdd(cart, false);
+            initializeCartRemove(cart, false);
+        })
+    }
+})();
